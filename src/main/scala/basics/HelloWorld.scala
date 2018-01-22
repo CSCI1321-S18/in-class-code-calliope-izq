f56e63925713d425da1d5482c62c994863c8104f
2017-08-23 <jizquie1@dias01.cs.trinity.edu>
2018-01-22 16:23:47 -0600

    college example and scalafx stuff

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -1,14 +1,20 @@
 package basics
 
+import io.StdIn._
+import scalafx.application.JFXApp
+
 /**
-This is a basic main for you to start off with.
-*/
-object HelloWorld {
-	def main(args: Array[String]): Unit = {
-		println("Hello World!")
-	}
-	
-	def square(x: Double) = x*x
-	
-	def cube(x: Double) = x*x*x
+ * This is a basic main for you to start off with.
+ */
+object HelloWorld extends JFXApp {
+//  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+    println("What is your name?")
+    val name = readLine
+    println("Hello, " + name + "!")
+//  }
+
+  def square(x: Double) = x * x
+
+  def cube(x: Double) = x * x * x
 }
